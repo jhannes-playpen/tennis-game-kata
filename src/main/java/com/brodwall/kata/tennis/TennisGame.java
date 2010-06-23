22325732ca96d96e7c6831773d39dff02583a64e
Johannes Brodwall <johannes@brodwall.com>
2010-06-24 00:12:23 +0200

    13 minutes

--- src/main/java/com/brodwall/kata/tennis/TennisGame.java
+++ src/main/java/com/brodwall/kata/tennis/TennisGame.java
@@ -2,4 +2,49 @@ package com.brodwall.kata.tennis;
 
 public class TennisGame {
 
+    private int playerOneBalls = 0;
+    private int playerTwoBalls = 0;
+
+    public void playerOneScores() {
+        playerOneBalls++;
+    }
+
+    public void playerTwoScores() {
+        playerTwoBalls++;
+    }
+
+    public String getScore() {
+        if (isWon()) return getLeader() + " Wins";
+        if (isDeuce()) return "Deuce";
+        if (isEndgame()) return "Advantage " + getLeader();
+        if (isEven()) return getScoreName(playerOneBalls) + " All";
+
+        return getScoreName(playerOneBalls) + " " + getScoreName(playerTwoBalls);
+    }
+
+    private boolean isDeuce() {
+        return isEndgame() && isEven();
+    }
+
+    private boolean isEven() {
+        return playerOneBalls == playerTwoBalls;
+    }
+
+    private boolean isWon() {
+        return isEndgame() && playerOneBalls > playerTwoBalls+1 || playerOneBalls+1 < playerTwoBalls;
+    }
+
+    private boolean isEndgame() {
+        return playerOneBalls >= 4;
+    }
+
+    private String getLeader() {
+        return playerOneBalls > playerTwoBalls ? "Player One" : "Player Two";
+    }
+
+    private String getScoreName(int score) {
+        String[] scoreNames = new String[] { "Love", "Fifteen", "Thirty", "Forty" };
+        return scoreNames[score];
+    }
+
 }
--- src/test/java/com/brodwall/kata/tennis/TennisScoreTest.java
+++ src/test/java/com/brodwall/kata/tennis/TennisScoreTest.java
@@ -1,5 +1,98 @@
 package com.brodwall.kata.tennis;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class TennisScoreTest {
+
+    private TennisGame game = new TennisGame();
+
+    @Test
+    public void shouldScoreLoveAll() throws Exception {
+        assertScore("Love All");
+    }
+
+    @Test
+    public void shouldScoreFifteenAll() throws Exception {
+        bothPlayersScore(1);
+        assertScore("Fifteen All");
+    }
+
+    @Test
+    public void shouldScoreThirtyAll() throws Exception {
+        bothPlayersScore(2);
+        assertScore("Thirty All");
+    }
+
+    @Test
+    public void shouldScoreFortyAll() throws Exception {
+        bothPlayersScore(3);
+        assertScore("Forty All");
+    }
+
+    @Test
+    public void shouldScoreDeuce() throws Exception {
+        bothPlayersScore(4);
+        assertScore("Deuce");
+    }
+
+    @Test
+    public void shouldScoreDeuceWhilePlayersHaveSameScore() throws Exception {
+        bothPlayersScore(100);
+        assertScore("Deuce");
+    }
+
+    @Test
+    public void shouldScoreFifteenLove() throws Exception {
+        game.playerOneScores();
+        assertScore("Fifteen Love");
+    }
+
+    @Test
+    public void shouldScoreLoveFifteen() throws Exception {
+        game.playerTwoScores();
+        assertScore("Love Fifteen");
+    }
+
+    @Test
+    public void shouldScoreAdvantagePlayerOne() throws Exception {
+        bothPlayersScore(4);
+        game.playerOneScores();
+        assertScore("Advantage Player One");
+    }
+
+    @Test
+    public void shouldScoreAdvantagePlayerTwo() throws Exception {
+        bothPlayersScore(4);
+        game.playerTwoScores();
+        assertScore("Advantage Player Two");
+    }
+
+    @Test
+    public void shouldScoreWinPlayerOne() throws Exception {
+        bothPlayersScore(4);
+        game.playerOneScores();
+        game.playerOneScores();
+        assertScore("Player One Wins");
+    }
+
+    @Test
+    public void shouldScoreWinPlayerTwo() throws Exception {
+        bothPlayersScore(4);
+        game.playerTwoScores();
+        game.playerTwoScores();
+        assertScore("Player Two Wins");
+    }
+
+    private void assertScore(String expectedScore) {
+        assertEquals(expectedScore, game.getScore());
+    }
+
+    private void bothPlayersScore(int times) {
+        for (int i=0; i<times; i++) {
+            game.playerOneScores(); game.playerTwoScores();
+        }
+    }
 
 }
