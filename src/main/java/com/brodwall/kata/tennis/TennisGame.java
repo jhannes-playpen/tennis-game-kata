e07174e476e32ff8c5741ed5c6bc4bf3c1773352
Johannes Brodwall <johannes@brodwall.com>
2010-11-24 23:46:01 +0100

    23 minutes

--- src/main/java/com/brodwall/kata/tennis/TennisGame.java
+++ src/main/java/com/brodwall/kata/tennis/TennisGame.java
@@ -2,4 +2,57 @@ package com.brodwall.kata.tennis;
 
 public class TennisGame {
 
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
+
+    public void playerOneScores() {
+        this.playerOneScore++;
+    }
+
+    public void playerTwoScores() {
+        this.playerTwoScore++;
+    }
+
+    public String getScore() {
+        if (isGameWon()) return getLeadingPlayer() + " wins";
+        if (isAdvantage()) return "advantage " + getLeadingPlayer();
+        if (isGameDeuce()) return "deuce";
+        if (isGameEven()) return getScoreName(playerOneScore) + "-all";
+        return getScoreName(playerOneScore) + "-" + getScoreName(playerTwoScore);
+    }
+
+    private boolean isGameDeuce() {
+        return isGameEven() && getHigherScore() >= 3;
+    }
+
+    private boolean isGameWon() {
+        return getHigherScore() >= 4 && getHigherScore() > getLowerScore() + 1;
+    }
+
+    private boolean isAdvantage() {
+        return getHigherScore() >= 4 && getHigherScore() == getLowerScore()+1;
+    }
+
+    private boolean isGameEven() {
+        return playerOneScore == playerTwoScore;
+    }
+
+    private String getScoreName(int score) {
+        String[] scoreNames = new String[] { "love", "fifteen", "thirty", "forty" };
+        return scoreNames[score];
+    }
+
+    private int getHigherScore() {
+        return playerOneScore > playerTwoScore ? playerOneScore : playerTwoScore;
+    }
+
+    private int getLowerScore() {
+        return playerOneScore > playerTwoScore ? playerTwoScore : playerOneScore;
+    }
+
+    private String getLeadingPlayer() {
+        return playerOneScore > playerTwoScore ? "player one" : "player two";
+    }
+
+
 }
--- src/test/java/com/brodwall/kata/tennis/TennisScoreTest.java
+++ src/test/java/com/brodwall/kata/tennis/TennisScoreTest.java
@@ -1,5 +1,95 @@
 package com.brodwall.kata.tennis;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class TennisScoreTest {
 
+    private TennisGame game = new TennisGame();
+
+    @Test
+    public void loveAll() throws Exception {
+        assertEquals("love-all", game.getScore());
+    }
+
+    @Test
+    public void fifteenAll() throws Exception {
+        bothPlayersScore(1);
+        assertEquals("fifteen-all", game.getScore());
+    }
+
+    @Test
+    public void thirtyAll() throws Exception {
+        bothPlayersScore(2);
+        assertEquals("thirty-all", game.getScore());
+    }
+
+    @Test
+    public void deuce() throws Exception {
+        bothPlayersScore(3);
+        assertEquals("deuce", game.getScore());
+        bothPlayersScore(100);
+        assertEquals("deuce", game.getScore());
+    }
+
+    @Test
+    public void fifteenLove() throws Exception {
+        game.playerOneScores();
+        assertEquals("fifteen-love", game.getScore());
+    }
+
+    @Test
+    public void fifteenThirty() throws Exception {
+        bothPlayersScore(1); game.playerTwoScores();
+        assertEquals("fifteen-thirty", game.getScore());
+    }
+
+    @Test
+    public void playerOneCleanWin() throws Exception {
+        game.playerOneScores(); game.playerOneScores();
+        game.playerOneScores(); game.playerOneScores();
+        assertEquals("player one wins", game.getScore());
+    }
+
+    @Test
+    public void playerTwoWinsWithoutDeuce() throws Exception {
+        bothPlayersScore(2);
+        game.playerTwoScores(); game.playerTwoScores();
+        assertEquals("player two wins", game.getScore());
+    }
+
+    @Test
+    public void advantagePlayerOne() throws Exception {
+        bothPlayersScore(3);
+        game.playerOneScores();
+        assertEquals("advantage player one", game.getScore());
+    }
+
+    @Test
+    public void advantagePlayerTwo() throws Exception {
+        bothPlayersScore(100);
+        game.playerTwoScores();
+        assertEquals("advantage player two", game.getScore());
+    }
+
+    @Test
+    public void playerOneWinsAfterLongTie() throws Exception {
+        bothPlayersScore(100);
+        game.playerOneScores(); game.playerOneScores();
+        assertEquals("player one wins", game.getScore());
+    }
+
+    @Test
+    public void thirtyForty() throws Exception {
+        bothPlayersScore(2);
+        game.playerTwoScores();
+        assertEquals("thirty-forty", game.getScore());
+    }
+
+    protected void bothPlayersScore(int times) {
+        for (int i=0; i<times; i++) {
+            game.playerOneScores(); game.playerTwoScores();
+        }
+    }
 }
